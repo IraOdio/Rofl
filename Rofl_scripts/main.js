4c5901eaa5787f0d3d2f624d518f890c49945680
IraOdio <spirits.ice@yandex.ru>
2019-08-04 20:50:03 +0300

    init commit

--- Rofl_scripts/main.js
+++ Rofl_scripts/main.js
@@ -7,9 +7,34 @@ include("Rofl_scripts/parallax.js");
         height: 300,
     });
     $("#flipbook").turn("zoom", 1.5);
-
+    sceneSnowMountain();
+    events();
 });
+function events(){
+    $("#flipbook").bind("turned", function(event, page, view) {
+        if(page==1){
+            sceneForest();
+            sceneSnowMountain();
+        }
+        if(page==2){
+            sceneSnowMountain();
+            sceneForest();
+        }
 
+    });
+}
+
+function sceneSnowMountain(){
+$("#IDbg").toggleClass("layer_SnowMountain");
+    $("#IDl1").toggleClass("layer_blueSky");
+    $("#IDl2").toggleClass("layer_clouds");
+}
+
+function sceneForest(){
+    $("#IDbg").toggleClass("layer_forest");
+    $("#IDl1").toggleClass("layer_ravens");
+    $("#IDl2").toggleClass("layer_SpiritForest");
+}
 
 //include function
 function include(url) {
--- Rofl_scripts/parallax.js
+++ Rofl_scripts/parallax.js
@@ -1,14 +1,13 @@
 function paralax(event){
     console.log(event);
     this.querySelectorAll('.layer').forEach(element=>{
-        //element.style.transform = `translateX(${event.clientX*  element.getAttribute('data-speed')/50}px)`;
         element.style.transform = `translate(${event.clientX*  element.getAttribute('data-speed')/50}px,
         ${event.clientY*  element.getAttribute('data-speed')/100}px)`;
     });
-    this.querySelectorAll('.layer_cloud').forEach(element=>{
+    this.querySelectorAll('.layer_1').forEach(element=>{
         element.style.transform = `translateY(${event.clientY*  element.getAttribute('data-speed')/900}px)`;
     });
-    this.querySelectorAll('.layer_skyBlue').forEach(element=>{
+    this.querySelectorAll('.layer_2').forEach(element=>{
         element.style.transform = `translateY(${event.clientY*  element.getAttribute('data-speed')/300}px)`;
     });
 }
